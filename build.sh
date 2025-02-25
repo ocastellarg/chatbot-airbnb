ee9b1c2b0a737e2a393de1d04889498e227121cf
Orlando Castellar <orlando.castellar@candcwireless.com>
2025-02-25 16:02:24 -0500

    Corrigiendo instalación de Google Chrome y ChromeDriver en Render

--- build.sh
+++ build.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 # Instalar Google Chrome y ChromeDriver en Render
 
-# Descargar e instalar Google Chrome
-wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+# Actualizar paquetes
 sudo apt-get update
-sudo apt-get install -y google-chrome-stable
+
+# Instalar dependencias necesarias
+sudo apt-get install -y wget unzip curl
+
+# Descargar e instalar Google Chrome
+wget -q -O google-chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome.deb || sudo apt-get -f install -y
+rm google-chrome.deb
+
+# Verificar instalación de Chrome
+google-chrome --version
 
 # Descargar e instalar ChromeDriver
-CHROME_VERSION=$(google-chrome-stable --version | awk '{print $3}')
+CHROME_VERSION=$(google-chrome --version | awk '{print $3}' | cut -d '.' -f1)
 CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
 wget "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
 unzip chromedriver_linux64.zip
 sudo mv chromedriver /usr/bin/chromedriver
 sudo chmod +x /usr/bin/chromedriver
+
+# Verificar instalación de ChromeDriver
+chromedriver --version
