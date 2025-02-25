1d3cec231d4e044ca6690ef1f6704fab5e520f2c
Orlando Castellar <orlando.castellar@candcwireless.com>
2025-02-25 12:55:26 -0500

    Añadiendo script para instalar Google Chrome en Render

---
+++ build.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# Instalar Google Chrome en Render
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
+sudo apt-get update
+sudo apt-get install -y google-chrome-stable
