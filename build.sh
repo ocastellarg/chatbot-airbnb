c46b84962f80c8aa447e73636cded673c1f4a258
Orlando Castellar <orlando.castellar@candcwireless.com>
2025-02-25 13:04:00 -0500

    Instalación manual de Chrome y ChromeDriver en Render

--- build.sh
+++ build.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
-# Instalar Google Chrome en Render
+# Instalar Google Chrome y ChromeDriver en Render
+
+# Descargar e instalar Google Chrome
 wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo apt-get update
 sudo apt-get install -y google-chrome-stable
+
+# Descargar e instalar ChromeDriver
+CHROME_VERSION=$(google-chrome-stable --version | awk '{print $3}')
+CHROMEDRIVER_VERSION=$(curl -s "https://chromedriver.storage.googleapis.com/LATEST_RELEASE_$CHROME_VERSION")
+wget "https://chromedriver.storage.googleapis.com/$CHROMEDRIVER_VERSION/chromedriver_linux64.zip"
+unzip chromedriver_linux64.zip
+sudo mv chromedriver /usr/bin/chromedriver
+sudo chmod +x /usr/bin/chromedriver
